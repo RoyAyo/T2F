32e830c02d787800b2c68dd1ea405ef93fec902e
Roy Ayo <roylayindeay0@gmail.com>
2025-02-10 21:06:32 +0100

    added some middlewares to prevent abuse

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 import os
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import StreamingResponse
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 from app.model.fart_model import FartModel
 
@@ -14,6 +20,10 @@ fartModel = FartModel()
 
 app = FastAPI()
 
+limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
+app.state.limiter = limiter
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://text2fart.com"],
@@ -21,6 +31,15 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+
+app.add_middleware(GZipMiddleware, minimum_size=1000) 
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["text2fart.com", "api.text2fart.com" , "localhost"])
+app.add_middleware(HTTPSRedirectMiddleware)
+
+@app.middleware("http")
+async def rate_limit_middleware(request: Request, call_next):
+    response = await limiter(request, call_next)
+    return response
 
 @app.get("/", status_code=200)
 def read_root():
