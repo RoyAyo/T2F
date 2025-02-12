a9b1c0b48ecbbbaef60279d7544769140932a219
Roy Ayo <roylayindeay0@gmail.com>
2025-02-13 00:27:46 +0100

    small fix

--- app/twitter/bot.py
+++ app/twitter/bot.py
@@ -1,26 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
-
-from app.utils.helper import convert_twitter_url_to_nitter
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://nitter.net/",
-    "DNT": "1",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-}
-
-session = requests.Session()
+import requests        
 
 def get_tweet_text(tweet_url):
     try:
-        nitter_url = convert_twitter_url_to_nitter(tweet_url)
-        response = session.get(nitter_url, headers=headers)
-        soup = BeautifulSoup(response.text, "html.parser")
-        tweet_text = soup.find("div", class_="tweet-content").text
+        url = f"https://proxy-jev0vg.fly.dev/tweet?url={tweet_url}"
+        response = requests.get(url)
+        tweet_text = response.text
         print("Tweet Text: ", tweet_text)
         return tweet_text
     except Exception as e:
